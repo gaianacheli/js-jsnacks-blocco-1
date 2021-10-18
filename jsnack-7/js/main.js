29cd01355084ff4583738c330056dda677d71531
gaianacheli <gaianacheli04@gmail.com>
2021-10-18 19:01:16 +0200

    JSNACK 7 COMPLETO

---
+++ jsnack-7/js/main.js
@@ -0,0 +1,10 @@
+//snack 7
+
+let risultato = 2
+const number = document.querySelector('.number')
+
+for (let i=0; i < 999;i++) {
+     risultato = risultato * 2
+     number.innerHTML+=(risultato + '<br>')
+
+}
