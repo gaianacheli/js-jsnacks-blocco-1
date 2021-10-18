c8ea6b768e089a64b5cd6121c4d6c09167fed201
gaianacheli <gaianacheli04@gmail.com>
2021-10-18 17:57:16 +0200

    JSNACK 4 completo

--- jsnack-4/js/main.js
+++ jsnack-4/js/main.js
@@ -1,2 +1,19 @@
 //snack 4
 
+const invitati  = [ "Giulia","Paolo","francesco","mario","monia","andrea" ]
+let invito = document.querySelector (".invito")
+
+
+const nameUser = prompt('Inserisci il tuo nome')
+
+
+let found = 'il tuo nome non è presente nella lista'
+
+for (let i=0; i < invitati.length; i++) {
+    if ( nameUser.toLowerCase() == invitati[i].toLowerCase()) {
+        found = 'Il tuo nome è presente nella lista'
+        console.log(found);
+    }
+}
+
+invito.innerHTML = found
