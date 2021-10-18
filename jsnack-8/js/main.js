9a1acd1390e35529cfe3bc8cacf6d86d80981357
gaianacheli <gaianacheli04@gmail.com>
2021-10-18 19:25:05 +0200

    JSNACK 8 completo

---
+++ jsnack-8/js/main.js
@@ -0,0 +1,11 @@
+//snack 8
+
+const somma = document.querySelector('.somma')
+let numberUser
+do {
+    numberUser= prompt ('Inserisci un numero') 
+}while(numberUser.length!=4)
+
+let risultato = parseInt(numberUser[0]) + parseInt(numberUser[1]) + parseInt(numberUser[2]) + parseInt(numberUser[3])
+
+somma.innerHTML= risultato
