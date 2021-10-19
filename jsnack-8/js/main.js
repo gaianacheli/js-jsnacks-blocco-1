34d318c4f37cb167bd6e71c034f3f2131eb75f11
gaianacheli <gaianacheli04@gmail.com>
2021-10-19 10:17:02 +0200

    snack-8

--- jsnack-8/js/main.js
+++ jsnack-8/js/main.js
@@ -3,7 +3,7 @@
 const somma = document.querySelector('.somma')
 let numberUser
 do {
-    numberUser= prompt ('Inserisci un numero') 
+    numberUser= prompt ('Inserisci un numero  di quattro cifre') 
 }while(numberUser.length!=4)
 
 let risultato = parseInt(numberUser[0]) + parseInt(numberUser[1]) + parseInt(numberUser[2]) + parseInt(numberUser[3])
